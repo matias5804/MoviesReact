8edaf7895d935aa80670b26471c4accd511bcbe4
matias5804 <88583185+matias5804@users.noreply.github.com>
2022-02-10 13:32:00 +0700

    movies terminado

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+import { Search } from "./components/Search";
+
+export function App() {
+  return (
+    <Router>
+      <header>
+        <Link to="/">
+          <h1 className={styles.title}>Movies</h1>
+        </Link>
+        <Search />
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/movies/:movieId">
+            <MovieDetails />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
+}
